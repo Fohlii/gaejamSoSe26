8febc96f729f2772a23e5db6022cf71254b733a4
Benedek Kovacs <3022716@stud.th-mannheim.de>
2026-06-02 12:43:48 +0200

    minor restructure

--- Scripts/GD-Scripts/UI/MainMenuBehaviour.gd
+++ Scripts/GD-Scripts/UI/MainMenuBehaviour.gd
@@ -6,6 +6,9 @@ extends Node
 @onready var settings: HBoxContainer = $Settings
 @onready var main_menu: HBoxContainer = $MainMenu
 
+@onready var present: Node = $"../PresentRoot"
+@onready var playerPS: PackedScene = preload("res://Scenes/Game-Elements/Player.tscn")
+
 
 func play_click_sound() -> void:
 	button_click.play()
@@ -14,12 +17,16 @@ func play_click_sound() -> void:
 
 func _on_play_button_pressed() -> void:
 	await play_click_sound()
-	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
-
+	var player: CharacterBody2D = playerPS.instantiate()
+	var playerCam: Camera2D = player.get_child(2)
+	player.position.y = -500
+	present.add_child(player)
+	playerCam.make_current()
+	self.get_parent().remove_child(self)
 
 func _on_settings_button_pressed() -> void:
 	await play_click_sound()
-	get_tree().change_scene_to_file("res://Scenes/Settings.tscn")
+	## get_tree().change_scene_to_file("res://Scenes/UI-Elements/Settings.tscn")
 	settings.visible = true
 	main_menu.visible = false
 	how_to_play.visible = false
