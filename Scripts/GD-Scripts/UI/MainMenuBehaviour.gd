272f2eb842697e35210817a1cf59239221f35d1c
Fohlii <fohli54@gmail.com>
2026-06-04 12:14:55 +0200

    change volume in Settings

--- Scripts/GD-Scripts/UI/MainMenuBehaviour.gd
+++ Scripts/GD-Scripts/UI/MainMenuBehaviour.gd
@@ -8,11 +8,37 @@ extends Node
 @onready var root_scene = get_tree().get_current_scene()
 @onready var present: Node = $"../PresentRoot"
 @onready var playerPS: PackedScene = preload("res://Scenes/Game-Elements/Player.tscn")
+@onready var game_settings= $"/root/GlobalVars"
+@onready var musicplayer: AudioStreamPlayer = $"../MusicPlayer"
+@onready var master_bus_index = AudioServer.get_bus_index("Master")
+@onready var sfx_bus_index = AudioServer.get_bus_index("SFX")
+
+## Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("MainMenu")
+	game_settings.musicVol = 100	
+	game_settings.sfxVol = 100
+	musicplayer.playing = true
+	var increaseVol:Tween = get_tree().create_tween()
+	increaseVol.tween_property(musicplayer,"volume_db",-80 + (15*pow(game_settings.musicVol,0.37)),1).set_trans(Tween.TRANS_QUAD)
+	await increaseVol.finished
+	increaseVol.kill()
+	var increaseSfxVol:Tween = get_tree().create_tween()
+	increaseSfxVol.tween_property(musicplayer,"volume_db",-80 + (15*pow(game_settings.sfxVol,0.37)),1).set_trans(Tween.TRANS_QUAD)
+	await increaseSfxVol.finished
+	increaseSfxVol.kill()
+	print("done")
+	print(musicplayer.volume_db)
+	$Settings/AspectRatioContainer/VBoxContainer/AspectRatioContainer/GridContainer/HSlider.volume_changed.connect(changeVol.bind())
+	$Settings/AspectRatioContainer/VBoxContainer/AspectRatioContainer/GridContainer/HSlider2.sfx_volume_changed.connect(changeVol.bind())
+## 
+func changeVol() -> void:
+	#musicplayer.volume_db = -80 + (15*pow(settings.musicVol,0.37))
+	AudioServer.set_bus_volume_db(master_bus_index,-80 + (15*pow(game_settings.musicVol,0.37)))
 
 func play_click_sound() -> void:
 	button_click.play()
 	await button_click.finished
-
 
 func _on_play_button_pressed() -> void:
 	await play_click_sound()
@@ -51,4 +77,9 @@ func _on_button_3_pressed() -> void:
 
 
 func _on_button_button_down() -> void:
+	pass # Replace with function body.
+
+
+func _on_h_slider_drag_ended(value_changed: bool) -> void:
+	
 	pass # Replace with function body.
