9350f669ee71834e1df43a56ef26d796f4e5a5d6
Benedek Kovacs <3022716@stud.th-mannheim.de>
2026-06-29 04:53:14 +0200

    minor restructure. added timeobjects in past and present for P2. no logic yet.

--- Scripts/GD-Scripts/Interactables/Building.gd
+++
@@ -1,8 +0,0 @@
-@abstract class_name Building extends Interactable
-
-func interact(item: Item) -> Item:
-	enter()
-	return item
-
-func enter() -> void:
-	pass
--- Scripts/GD-Scripts/Interactables/Timeobjects/sapling.gd
+++ Scripts/GD-Scripts/Interactables/Timeobjects/sapling.gd
@@ -7,7 +7,7 @@ func _init() -> void:
 
 func _ready() -> void:
 	var saplingNotTaken: TimeobjectState = TimeobjectState.new("saplingNotTaken", Vector2(4375.0,-324.0), 0, 1.0, true, "sapling.png", Vector2(15,-35))
-	var saplingTaken: TimeobjectState = TimeobjectState.new("saplingTaken", Vector2(4375.0,-324.0), 0, 1.0, true, "", Vector2.ZERO, [], "sapling.tres")
+	var saplingTaken: TimeobjectState = TimeobjectState.new("saplingTaken", Vector2(4375.0,-324.0), 0, 1.0, true, "", Vector2.ZERO, [], "P1_sapling.tres")
 	saplingNotTaken.addInteractionTransition("EMPTY_HAND", saplingTaken.id)
 	
 	_statesById.set(saplingNotTaken.id, saplingNotTaken)
