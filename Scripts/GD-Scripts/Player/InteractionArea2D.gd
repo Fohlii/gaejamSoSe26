aa1b76094036f012c4dd223d0a5222b0c3418890
Fohlii <fohli54@gmail.com>
2026-06-30 14:57:27 +0200

    größere interaktionspunkte

--- Scripts/GD-Scripts/Player/InteractionArea2D.gd
+++ Scripts/GD-Scripts/Player/InteractionArea2D.gd
@@ -4,7 +4,7 @@ class_name InteractionArea2D extends Area2D
 
 func _ready() -> void:
 	collider.shape = CircleShape2D.new()
-	collider.shape.set_radius(20.0)
+	collider.shape.set_radius(40.0)
 	set_collision_layer_value(2, true)
 	monitoring = false
 	monitorable = true
