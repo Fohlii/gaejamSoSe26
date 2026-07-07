5b3ed738c3f0787a0aad9ab69947ad4af4db5ee1
Benedek Kovacs <3022716@stud.th-mannheim.de>
2026-07-07 07:51:18 +0200

    attempted fix for timetravel while climbing. No opportunity to test yet.

--- Scripts/GD-Scripts/Player/PlayerCharacterBody2D.gd
+++ Scripts/GD-Scripts/Player/PlayerCharacterBody2D.gd
@@ -324,6 +324,7 @@ class PlayerTimetravel extends PlayerComponent:
 		if Input.is_action_just_pressed("timetravel"):
 			player.get_tree().current_scene.toggle_time()
 			player.global_position.y += ((20000-player.TELEPORT_BUFFER) if (GlobalVars.player_in_past) else (-20000-player.TELEPORT_BUFFER))
+			player.playerMovement.changeState("FallingMotionState") ## Attempted Fix for timetravel while on ladder
 
 class PlayerInteraction extends PlayerComponent:
 	var playerInteractionArea: Area2D
