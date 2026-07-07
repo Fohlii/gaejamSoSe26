e3d2802a6d97757052abea6fb198774d21ab92c1
Fohlii <fohli54@gmail.com>
2026-07-07 13:16:00 +0200

    grobes gewinn-screen und auslösung => spieler kann theoretisch gewinnen ist noch nicht schön aber funktioniert

--- Scripts/GD-Scripts/Autoload/GameManager.gd
+++ Scripts/GD-Scripts/Autoload/GameManager.gd
@@ -11,6 +11,7 @@ extends Node
 @onready var h_slider_2: HSlider = $Settings/AspectRatioContainer/VBoxContainer/AspectRatioContainer/GridContainer/HSlider2
 @onready var game_over: CanvasLayer = $UI/GameOver
 @onready var inventory_overlay: CanvasLayer = $UI/InventoryOverlay
+@onready var game_won: CanvasLayer = $UI/GameWon
 
 @onready var timeobjectManager: TimeobjectManager = TimeobjectManager.new()
 
@@ -23,7 +24,7 @@ func _ready() -> void:
 	print("present_root: ", present_root)
 	print("main_menu: ", main_menu)
 	Dialogic.signal_event.connect(game_lost)
-	
+	GlobalVars.winning_condition.connect(player_won)
 func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("esc"):
 			if main_menu.visible:
@@ -39,10 +40,12 @@ func toggle_time() -> void:
 	print("Is in past: " + str(GlobalVars.player_in_past))
 
 func start_game():
+	GlobalVars.player_won = false
 	Dialogic.VAR.playing = true
 	timeobjectManager.initializeTimeobjects(get_tree())
 	main_menu.visible = false
 	game_over.visible = false
+	game_won.visible = false
 	if player== null:
 		player =  playerPS.instantiate()
 	add_child(player)
@@ -60,3 +63,5 @@ func show_menu():
 func game_lost(argument):
 	if argument == "player_lost":
 		game_over.visible = true
+func player_won():
+	game_won.visible = true
